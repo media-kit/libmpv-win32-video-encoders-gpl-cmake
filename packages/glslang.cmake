5de5d84adaeef7e9276e7d57c1609a0af3cac769
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-08-25 19:37:30 +0530

    fix: pin glslang version

--- packages/glslang.cmake
+++ packages/glslang.cmake
@@ -1,9 +1,8 @@
 ExternalProject_Add(glslang
     GIT_REPOSITORY https://github.com/KhronosGroup/glslang.git
     SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
     GIT_REMOTE_NAME origin
-    GIT_TAG main
+    GIT_TAG 13.0.0
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
